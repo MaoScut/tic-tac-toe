1da3cd76fc31cdeb47881739a4a12219285ec47d
MaoScut <849345372@qq.com>
2017-11-04 19:47:39 +0800

    补齐分号

--- src/index.jsx
+++ src/index.jsx
@@ -49,13 +49,13 @@ class Game extends React.Component {
       }]),
       stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
-    })
+    });
   }
   jumpTo(step) {
     this.setState({
       stepNumber: step,
       xIsNext: (step) % 2 === 0,
-    })
+    });
   }
   render() {
     const history = this.state.history;
@@ -70,7 +70,7 @@ class Game extends React.Component {
           <button onClick={() => this.jumpTo(move)}>{desc}</button>
         </li>
       )
-    })
+    });
     let status;
     if (winner) {
       status = "Winner:" + winner;
